2c2efe628d3f00a9c3aac001d6876e457d3017cd
Jalmer van de Berg <jalmervandeberg@sogyo.nl>
2016-09-14 15:47:55 +0200

    First commit

---
+++ src/typechecking.hs
@@ -0,0 +1,29 @@
+module TypeChecking where
+
+import Data.List (intercalate)
+import qualified Data.Map as M
+
+type Ident = String
+type TVar = String
+
+data Type
+  = Prim TVar
+  | Var TVar
+  | Prod [Type]
+  | Arrow Type Type
+  deriving Eq
+
+instance Show Type where
+  show (Prim x) = x
+  show (Var x) = "(" ++ x ++ ")"
+  show (Prod xs) = "(" ++ (intercalate " X " (map show xs)) ++ ")"
+  show (Arrow t1 t2) = "(" ++ (show t1) ++ " -> " ++ (show t2) ++ ")"
+  
+
+data Expr
+  = Ident Ident
+  | FunDef Ident Ident Expr
+  | FunApp Ident Ident
+  deriving (Eq, Show)
+
+type Env = M.Map Ident Type
